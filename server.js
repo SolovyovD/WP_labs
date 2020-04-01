0d68db87aac685d0982a30a6fbb9caaf9a8930ff
SolovyovD <antoniobmwsolov@gmail.com>
2020-04-01 21:27:01 +0300

    Completed the third lab

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-//Подключение http-модуля,
-//который поставляется вместе с Node.js и
-//реализация доступа к нему через переменную http
-var http = require("http");
-//Подключение модуля для работы с файловой системой
-var fs = require("fs");
-//Создание http-сервера, использующего порт 8888
-http.createServer(function (request, response) {
-    console.log("Request received");
-    //загрузка html страницы из файловой системы
-    var page = fs.readFileSync('index.html');
-    //запись в ответ заголовка с указанием кода состояния, и типа возвращаемого контента
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    //тело ответа
-    response.write(page);
-    //отправка ответа
-    response.end();
-}).listen(8888);
-console.log("Server has started");
+const fs = require("fs"); 
+const http = require("http");
+const url = require('url');
+const querystring = require('querystring');
+
+function start() {
+    function onRequest(request, response) { 
+        console.log("Request received"); 
+        var sum = 0;
+        var sp = url.parse(request.url).search;
+        if (sp != null) {
+            var parsed_sp = querystring.parse(sp.split('?')[1]);
+            for (key in parsed_sp) {
+                if (Number.isInteger(Number(parsed_sp[key]))) {
+                    sum += Number(parsed_sp[key]);
+                }
+            }
+            console.log("Result = " + sum);
+        }
+        if (sum != 0) {
+            response.write(sum.toString());
+            response.end();
+            return;
+        }
+        var page = fs.readFileSync('index.html'); 
+        response.writeHead(200, { 'Content-Type': 'text/html' }); 
+        response.write(page);
+        response.end(); 
+    }
+    http.createServer(onRequest).listen(8888); 
+    console.log("Server has started");
+}
+
+exports.start = start;
