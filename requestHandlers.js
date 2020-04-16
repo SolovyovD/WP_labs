44cf20f693b5dddaf9f2f15528be63ddbbdbcafc
SolovyovD <antoniobmwsolov@gmail.com>
2020-04-16 16:59:57 +0300

    Complete the fifth lab

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,78 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
+var fs = require("fs");
 
-function start(response) {
-    console.log("Request handler 'start' was called.");
-    exec("ls -lah", function (error, stdout, stderr) {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write(stdout);
-        response.end();
-    });
+function getRandomInt(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function upload(response) {
+function start(response, postData) {
+    console.log("Request handler 'start' was called.");
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<input pattern="[a-zA-Z]{1,8}\\s?\\d?" name="text">' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(body);
+    response.end();
+}
+
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello Upload");
+    var req = querystring.parse(postData).text.trim().split(' ');
+    switch(req[0]) {
+        case 'kaban':
+            kaban(response, req)
+            break;
+        case 'kakadu':
+            kakadu(response, req);
+            break;
+        case 'kalmar':
+            kalmar(response, req);
+            break;
+        default:
+            start(response, postData)
+    }
+}
+
+function kaban(response, Data) {
+    if (typeof Data[1] === 'undefined') {
+        Data[1] = getRandomInt(0,9);
+    }
+    var i = Data[1];
+    var image = fs.readFileSync('images/kaban/kaban_'+i+'.jpg');
+    response.writeHead(200, { "Content-Type": "image/jpg" });
+    response.write(image);
+    response.end();
+}
+
+function kakadu(response, Data) {
+    if (typeof Data[1] === 'undefined') {
+        Data[1] = getRandomInt(0,9);
+    }
+    var i = Data[1];
+    var image = fs.readFileSync('images/kakadu/kakadu_'+i+'.jpg');
+    response.writeHead(200, { "Content-Type": "image/jpg" });
+    response.write(image);
+    response.end();
+}
+
+function kalmar(response, Data) {
+    if (typeof Data[1] === 'undefined') {
+        Data[1] = getRandomInt(0,9);
+    }
+    var i = Data[1];
+    var image = fs.readFileSync('images/kalmar/kalmar_'+i+'.jpg');
+    response.writeHead(200, { "Content-Type": "image/jpg" });
+    response.write(image);
     response.end();
 }
 
--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const http = require("http");
 const url = require('url');
 
 function start(route, handle) {
-    function onRequest(request, response) { 
+    function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received");
-        route(handle, pathname, response);        
+        console.log("Request for " + pathname + " received.");
+        request.setEncoding("utf8");
+        request.addListener("data", function (postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" +
+                postDataChunk + "'.");
+        });
+        request.addListener("end", function () {
+            console.log(pathname);
+            route(handle, pathname, response, postData);
+        });
     }
-    http.createServer(onRequest).listen(8888); 
+    http.createServer(onRequest).listen(8888);
     console.log("Server has started");
 }
 
