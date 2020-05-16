8f5c2b4267dc537f62aae91006ccc6f8f9336838
SolovyovD <antoniobmwsolov@gmail.com>
2020-05-16 21:14:46 +0300

    Correct the first request

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require("fs");
 const router = express.Router();
 
 const colors = ['#FF0000', '#00FF00', '#FF8000']
@@ -9,30 +10,10 @@ function getRandomInt(min, max) {
 }
 
 router.get('/', (req, res) => {
-    res.send('<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body>' +
-        '<form action="/getPicture" method="post">' +
-        '<select name="colorSelector">' +
-        '<option hidden>Выберите цвет</option>' +
-        '<option>Красный</option>' +
-        '<option>Зеленый</option>' +
-        '<option>Оранжевый</option>' +
-        '</select>' +
-        '<select name="sizeSelector">' +
-        '<option hidden>Выберите размер</option>' +
-        '<option>400х600</option>' +
-        '<option>800х600</option>' +
-        '<option>1280х960</option>' +
-        '</select>' +
-        '<input pattern="[a-zA-Z]{1,8}\\s?\\d?" name="text" required>' +
-        '<input type="submit" value="Submit text" />' +
-        '</form>' +
-        '</body>' +
-        '</html>');
+    let body = fs.readFileSync('index.html');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(body);
+    res.end();
 })
 
 router.post('/getPicture', (req, res) => {
@@ -40,7 +21,10 @@ router.post('/getPicture', (req, res) => {
     if (req.body.sizeSelector == 'Выберите размер') {
         splitSizes = sizes[getRandomInt(0, 2)].split('x');
     }
-    else { splitSizes = req.body.sizeSelector.split('х'); }
+    else {
+        splitSizes = req.body.sizeSelector.split('x');
+        console.log(req.body.sizeSelector);
+    }
     let params = {
         name: req.body.text.trim().split(' '),
         color: req.body.colorSelector,
@@ -50,8 +34,10 @@ router.post('/getPicture', (req, res) => {
         },
         image: 'default',
     }
-    if (params.name[0] != 'kaban' | 'kalmar' | 'kakadu') {
-        res.send('not found');
+    if (params.name[0] != 'kaban' &
+        params.name[0] != 'kalmar' &
+        params.name[0] != 'kakadu') {
+        res.send({ name: 'not found' });
         return;
     }
     if (typeof params.name[1] === 'undefined') {
@@ -79,7 +65,7 @@ router.get('/picture/:name/:id/:color/:size', (req, res) => {
         },
         id: req.params.id
     }
-    if (!Number.isInteger(imageParams.id) || imageParams.id > 9) {
+    if (Number.isInteger(imageParams.id) || Number(imageParams.id) > 9) {
         imageParams.id = getRandomInt(0, 9);
     }
     if (imageParams.name != 'kaban' &&
@@ -93,17 +79,14 @@ router.get('/picture/:name/:id/:color/:size', (req, res) => {
         imageParams.color != 'orange') {
         imageParams.color = colors[getRandomInt(0, 2)];
     }
-    res.send('<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body style="background: ' + imageParams.color + ';">' +
-        '<image width="' + imageParams.size.width +
-        ' height="' + imageParams.size.height +
-        '" src="/' + imageParams.name + '/' + imageParams.name + '_' + imageParams.id + '.jpg">' +
-        '</body>' +
-        '</html>');
+    res.send({
+        page:
+            '<div style="background: ' + imageParams.color + ';">' +
+            '<image width="' + imageParams.size.width +
+            ' height="' + imageParams.size.height +
+            '" src="http://localhost:8888/' + imageParams.name + '/' + imageParams.name + '_' + imageParams.id + '.jpg">' +
+            '</div>'
+    });
 })
 
 module.exports = router;
